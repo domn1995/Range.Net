bbf2a90353ef5dbcb90137b582a450ba0d763fcb
Domn Werner <wenr0096@vandals.uidaho.edu>
2018-10-28 23:33:43 -0700

    Documentation.

--- Range.Library/BaseRange.cs
+++ Range.Library/BaseRange.cs
@@ -3,17 +3,36 @@ using System.Collections.Generic;
 
 namespace Range.Library
 {
+    /// <summary>
+    /// An abstract representation of a range that can be enumerated and is defined with a string.
+    /// </summary>
     public abstract class BaseRange : IEnumerable<string>
     {
+        /// <summary>
+        /// Gets the original range string.
+        /// </summary>
         protected string Range { get; }
 
+        /// <summary>
+        /// Initializes a new <see cref="BaseRange"/> instance.
+        /// </summary>
+        /// <param name="range"></param>
         protected BaseRange(string range)
         {
             Range = range;
         }
 
+        /// <summary>
+        /// Gets this range's enumerator, providing foreach support.
+        /// </summary>
+        /// <returns>A string enumerator for this range.</returns>
         public abstract IEnumerator<string> GetEnumerator();
 
+        /// <inheritdoc />
+        /// <summary>
+        /// Gets this range's enumerator, providing foreach support.
+        /// </summary>
+        /// <returns>A string enumerator for this range.</returns>
         IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
     }
 }
--- Range.Library/DashRange.cs
+++ Range.Library/DashRange.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text.RegularExpressions;
 
 namespace Range.Library
 {
+    /// <summary>
+    /// Defines a range of values separated by dashes.
+    /// </summary>
+    /// <example>1-3 will give an enumerator that returns "1", "2", and "3".</example>
     public class DashRange : BaseRange
     {
         private static readonly Regex regex = new Regex(@"(-?\d+)-?(-?\d+)?", RegexOptions.Compiled);
@@ -12,8 +15,15 @@ namespace Range.Library
         private readonly int start;
         private readonly int end;
 
+        /// <inheritdoc />
+        /// <summary>
+        /// Initializes a new <see cref="T:Range.Library.DashRange" /> instance.
+        /// </summary>
+        /// <param name="range"></param>
         public DashRange(string range) : base(range)
         {
+            // If the input is blank, our enumerator shouldn't even loop once.
+            // Thus, set the end to before the start.
             if (string.IsNullOrWhiteSpace(range))
             {
                 end = -1;
@@ -24,21 +34,37 @@ namespace Range.Library
             Match match = regex.Match(range);
             GroupCollection groups = match.Groups;
             start = int.Parse(groups[1].Value);
+
+            // If groups[2].Success is true, that means we found two numbers, one on each side of the dash.
+            // Ex: "1-3".
+            // If not successful, we were given only one number so set the end equal to start.
+            // Ex: "1" or "1-".
             end = !groups[2].Success ? start : int.Parse(groups[2].Value);
         }
 
+        /// <inheritdoc />
         public override IEnumerator<string> GetEnumerator() => new DashRangeEnumerator(start, end);
 
+        /// <summary>
+        /// Defines an enumerator for dash ranges.
+        /// </summary>
         private struct DashRangeEnumerator : IEnumerator<string>
         {
             private readonly int start;
             private int index;
             private readonly int end;
 
+            /// <inheritdoc />
             public string Current { get; private set; }
 
+            /// <inheritdoc />
             object IEnumerator.Current => Current;
 
+            /// <summary>
+            /// Initializes a new <see cref="DashRangeEnumerator"/> instance.
+            /// </summary>
+            /// <param name="start">The range's start value.</param>
+            /// <param name="end">The range's end value.</param>
             public DashRangeEnumerator(int start, int end)
             {
                 Current = "";
@@ -46,6 +72,7 @@ namespace Range.Library
                 this.end = end;
             }
 
+            /// <inheritdoc />
             public bool MoveNext()
             {
                 if (index <= end)
@@ -58,12 +85,14 @@ namespace Range.Library
                 return false;
             }
 
+            /// <inheritdoc />
             public void Reset()
             {
                 index = start;
                 Current = "";
             }
 
+            /// <inheritdoc />
             public void Dispose() { }
         }
     }
